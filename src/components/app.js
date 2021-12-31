799c4100e1aa1b113af385f6dcecd93f3a3caa73
Mike Carouchi <mikecarouchi@gmail.com>
2021-12-31 07:45:11 -0800

    added logout functions without api call as well as initial blog page functions

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,96 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Auth from "./pages/auth";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+import Auth from "./pages/auth";
+import Blog from "./pages/blog";
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			loggedInStatus: "NOT_LOGGED_IN",
+		};
+
+		this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+		this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+		this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
+	}
+
+	handleSuccessfulLogin() {
+		this.setState({
+			loggedInStatus: "LOGGED_IN",
+		});
+	}
+
+	handleUnsuccessfulLogin() {
+		this.setState({
+			loggedInStatus: "NOT_LOGGED_IN",
+		});
+	}
+
+	handleSuccessfulLogout() {
+		this.setState({
+			loggedInStatus: "NOT_LOGGED_IN",
+		});
+	}
+
+	checkLoginStatus() {
+		return axios
+			.get("", {
+				withCredentials: true,
+			})
+			.then((response) => {
+				const loggedIn = response.data.logged_in;
+				const loggedInStatus = this.state.loggedInStatus;
+
+				if (loggedIn && loggedInStatus === "LOGGED_IN") {
+					return loggedIn;
+				} else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+					this.setState({
+						loggedInStatus: "LOGGED_IN",
+					});
+				} else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+					this.setState({
+						loggedInStatus: "NOT_LOGGED_IN",
+					});
+				}
+			})
+			.catch((error) => {
+				console.log("Error", error);
+			});
+	}
+
+	componentDidMount() {
+		this.checkLoginStatus();
+	}
+
+	authorizedPages() {
+		return [<Route path="/blog" component={Blog} />]; //POSSIBLY REMOVE
+	}
+
 	render() {
 		return (
 			<div className="container">
 				<Router>
 					<div>
 						<Switch>
-							<Route exact path="/auth" component={Auth} />
+							<Route path="/blog" component={Blog} />
+							<Route
+								exact
+								path="/"
+								render={(props) => (
+									<Auth
+										{...props}
+										handleSuccessfulLogin={this.handleSuccessfulLogin}
+										handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+									/>
+								)}
+							/>
+							{/* {this.state.loggedInStatus === "LOGGED_IN"
+								? this.authorizedPages()
+								: null} */}
 						</Switch>
 					</div>
 				</Router>
---
+++ src/components/pages/blog.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+class Blog extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			blogItems: [],
+		};
+
+		this.getBlogItems = this.getBlogItems.bind(this);
+	}
+
+	getBlogItems() {
+		axios
+			.get("https://mikecarouchi.devcamp.space/portfolio/portfolio_blogs", {
+				withCredentials: true,
+			})
+			.then((response) => {
+				this.setState({
+					blogItems: response.data.portfolio_blogs,
+				});
+			})
+			.catch((error) => {
+				console.log("getBlogItems error", error);
+			});
+	}
+
+	componentWillMount() {
+		this.getBlogItems();
+	}
+
+	render() {
+		const blogRecords = this.state.blogItems.map((blogItem) => {
+			return <h1>{blogItem.title}</h1>;
+		});
+		return <div className="blog">{blogRecords}</div>;
+	}
+}
+
+export default Blog;
